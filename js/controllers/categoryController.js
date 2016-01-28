0b689e98c9d87864a741203a742f4c42ccdaaca2
unknown <svs@WS-SVS1.dk.sitecore.net>
2016-01-28 13:59:52 +0200

    fixed filter

--- js/controllers/categoryController.js
+++ js/controllers/categoryController.js
@@ -8,55 +8,56 @@ define( function() {
         $scope.breadcrumb = [];
         $scope.numPerPage = 10;
         $scope.$state = $state;
-        var currentPage  = $state.params.page || 1;
-
-        $scope.selectBrand = {
-            availableOptions: [
-                {id: '', name: 'Выберете бренд'},
-                {id: 'hays', name: 'Hays'},
-                {id: 'cocoon', name: 'Cocoon'},
-                {id: 'virginia_secret', name: 'Virginia Secret'}
-            ],
-            change: function(){
-                var selected = $scope.selectBrand.selected.id;
-                $location.search("brand", selected ? selected : null);
-                $location.search("page", null);
-            },
-            selected: ""
-        };
+        var currentPage  = $state.params.page || 1;	
 		
-		$scope.selectSize = {
-            availableOptions: [
-                {id: '', name: 'Выберете размер'},
-                {id: 's', name: 'S'},
-                {id: 'm', name: 'M'},
-                {id: 'l', name: 'L'},
-				{id: 'xl', name: 'XL'},
-				{id: 'xxl', name: 'XXL'},
-				{id: 'xxxl', name: 'XXXL'},
-				{id: 'xxxxl', name: 'XXXXL'}
-            ],
-            change: function(){
-                var selected = $scope.selectSize.selected.id;
-                $location.search("size", selected ? selected : null);
-                $location.search("page", null);
-            },
-            selected: ""
-        };
-
-        $scope.sort = function(type) {
-            $location.search("sort", type);
-        };
-
-        $scope.selectBrand.availableOptions.forEach(function(index, i) {
+		$scope.filter = {
+			selectBrand : {
+				availableOptions: [
+					{id: '', name: 'Выберете бренд'},
+					{id: 'hays', name: 'Hays'},
+					{id: 'cocoon', name: 'Cocoon'},
+					{id: 'virginia_secret', name: 'Virginia Secret'}
+				],
+				change: function(){
+					var selected = $scope.filter.selectBrand.selected.id;
+					$location.search("brand", selected ? selected : null);
+					$location.search("page", null);
+				},
+				selected: ""
+			},		
+			selectSize : {
+				availableOptions: [
+					{id: '', name: 'Выберете размер'},
+					{id: 's', name: 'S'},
+					{id: 'm', name: 'M'},
+					{id: 'l', name: 'L'},
+					{id: 'xl', name: 'XL'},
+					{id: 'xxl', name: 'XXL'},
+					{id: 'xxxl', name: 'XXXL'},
+					{id: 'xxxxl', name: 'XXXXL'}
+				],
+				change: function(){
+					var selected = $scope.filter.selectSize.selected.id;
+					$location.search("size", selected ? selected : null);
+					$location.search("page", null);
+				},
+				selected: ""
+			},			
+			sort: function(type) {
+				$location.search("sort", type);
+			}			
+		};
+		
+		$scope.filter.selectBrand.availableOptions.forEach(function(index, i) {
             index.id ==  $state.params.brand ?
-                $scope.selectBrand.selected = $scope.selectBrand.availableOptions[i] : null;
+                $scope.filter.selectBrand.selected = $scope.filter.selectBrand.availableOptions[i] : null;
         });
 
-        $scope.selectSize.availableOptions.forEach(function(index, i) {
+        $scope.filter.selectSize.availableOptions.forEach(function(index, i) {
             index.id ==  $state.params.size ?
-                $scope.selectSize.selected = $scope.selectSize.availableOptions[i] : null;
+                $scope.filter.selectSize.selected = $scope.filter.selectSize.availableOptions[i] : null;
         });
+		
 
 		// if page = 1, will remove it from url
 		$scope.setPageSearch = function(currentPage){
