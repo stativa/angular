2772040193c3854d19874263bcc0b3b3db4d278f
unknown <svs@WS-SVS1.dk.sitecore.net>
2016-02-10 09:56:57 +0200

    added node

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     nodemon = require('gulp-nodemon');
  
+ /*
 gulp.task('connect', function() {
   connect.server({
     root: '',
-    port: 3000,
+    port: 3001,
     livereload: true,
     fallback: 'index.html'
   });
@@ -15,29 +16,17 @@ gulp.task('html', function () {
   gulp.src('./app/*.html')
     .pipe(connect.reload());
 });
- 
+
 gulp.task('watch', function () {
   gulp.watch(['./app/*'], ['html','js']);
-
-});
-
-
-gulp.task('nodemon', function () {
-  nodemon({ script: 'node/index.js'
-    , ext: 'html js' })
-      .on('restart', function () {
-        console.log('restarted!')
-      })
-});
-
-
+});*/
 
 gulp.task( 'server', function () {
   nodemon({
     script:     'node/index.js',
     env:        { 'NODE_ENV': 'development' },
-    watch:      ['./views', './controllers', './models','./libs'],
-    ext:        'html,js',
+    watch:      ['css', 'js', 'templates','node'],
+    ext:        'html,js,css',
     delay:      2,
     nodeArgs:   ['--debug']
   })
@@ -49,6 +38,4 @@ gulp.task( 'server', function () {
       });
 });
 
-
-
-gulp.task('default', ['connect', 'watch', 'server']);
+gulp.task('default', ['server']);
--- node/server.js
+++ node/server.js
@@ -1,17 +1,22 @@
 var http = require("http");
 var url = require("url");
+var express = require('express');
+var app = express();
+app.use(express.static('./'));
 
-function start() {
-  function onRequest(request, response) {
+function start() { 
+  app.get("/api/hello", function(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
-  }
+  });
 
-  http.createServer(onRequest);
+  http.createServer(app).listen(3000);
   console.log("Server has started.");
+
+  
 }
 
 exports.start = start;
